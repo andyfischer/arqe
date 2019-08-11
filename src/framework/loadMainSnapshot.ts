538ae0c36a0bdc5f73ef5696a52af1cd5c5b13aa
Andrew Fischer <andy.fischer@gmail.com>
2019-08-11 12:16:27 -0700

    move 'bootstrap' to 'data'

--- src/framework/loadMainSnapshot.ts
+++ src/framework/loadMainSnapshot.ts
@@ -9,10 +9,10 @@ export default async function loadMainSnapshot() {
     if (!_mainSnapshot) {
         const snapshot = new Snapshot();
 
-        await loadBootstrapFile(snapshot, `${__dirname}/../../bootstrap/_prelude.p`);
+        await loadBootstrapFile(snapshot, `${__dirname}/../../data/_bootstrap.p`);
 
         for (const script in snapshot.getValue('bootstrapScripts').scripts)
-            await loadBootstrapFile(snapshot, `${__dirname}/../../bootstrap/${script}`);
+            await loadBootstrapFile(snapshot, `${__dirname}/../../data/${script}`);
 
         const userEnv = path.join(os.homedir(), '.futureshell/env.p');
         await loadBootstrapFile(snapshot, userEnv);
