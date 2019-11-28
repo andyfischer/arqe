d239a5f2f5b8055940bcf3f8cda28615c276dc9f
Andrew Fischer <andy.fischer@gmail.com>
2019-11-28 14:54:37 -0700

    add testScripts

--- src/tags-db/FunctionalTests.ts
+++ src/tags-db/FunctionalTests.ts
@@ -1,4 +1,6 @@
 
+import Fs from 'fs'
+import Path from 'path'
 import CommandConnection from './CommandConnection'
 import { TestSession, TestSuite } from '../test-framework'
 
@@ -8,108 +10,24 @@ function fromScript(script: string) {
     })
 }
 
-const saveAndLoadTests = [
-fromScript(`
-    get a/x
-    expect #null
-`),
-fromScript(`
-    save a/x
-    get a/x
-    expect #exists
-`),
-fromScript(`
-    save a/x b/y
-    get b/y a/x
-    expect #exists
-`),
-]
-
-const contextTests = [
-fromScript(`
-    get a/top
-    expect #null
-    save a/top
-    get a/isolated
-    expect #null
-    context isolation
-    get a/isolated
-    expect #null
-    save a/isolated
-    get a/isolated
-    expect #exists
-    context -isolation
-    get a/top
-    expect #exists
-    get a/isolated
-    expect #null
-`),
-fromScript(`
-    context -nonexistanttag
-`),
-fromScript(`
-    save lower/a a/1
-    context lower/a
-    get a/1
-    expect #exists
-    context -lower/a
-`)]
-
-const getMultiTests = [
-fromScript(`
-    get a/*
-    expect []
-`),
-fromScript(`
-    save a/1
-    get a/*
-    expect [1]
-`),
-fromScript(`
-    save a/1
-    save a/2
-    get a/*
-    expect [1, 2]
-    save a/3
-    get a/*
-    expect [1, 2, 3]
-`)]
-
-const getMultiWithFixed = [
-fromScript(`
-    save a/1 b/1
-    save a/2 b/1
-    save a/3 b/2
-    get a/* b/1
-    expect [1, 2]
-`),
-fromScript(`
-    save a/1 b/1
-    save a/2 b/1
-    save a/3 b/2
-    get a/* b/2
-    expect [3]
-`),
-fromScript(`
-    save a/1 b
-    save a/2 b
-    save a/3 c
-    get a/* b
-    expect [1, 2]
-    get a/* c
-    expect [3]
-`)]
+function loadCasesFromFiles() {
+    const testDir = __dirname + '/../../testScripts/tags-db';
+    const files = Fs.readdirSync(testDir)
+    return files.map(file => {
+        const filename = Path.join(testDir, file);
+        const contents = Fs.readFileSync(filename, 'utf8');
+        return fromScript(contents);
+    });
+}
 
 export async function mainFunctionalTests(conn: CommandConnection) {
 
     const suite = new TestSuite();
     suite.conn = conn;
-    await suite.runAll([
-        ...saveAndLoadTests,
-        ...contextTests,
-        ...getMultiTests,
-        ...getMultiWithFixed
-    ])
+
+
+    await suite.runAll(loadCasesFromFiles());
+    
     const session = new TestSession();
     session.conn = conn;
 }
