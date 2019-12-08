75c3612dffb2a3a2667ac6df786d64fb66787149
Andrew Fischer <andy.fischer@gmail.com>
2019-12-08 11:06:52 -0500

    add cli option to run a single command

--- src/tags-db/startCli.ts
+++ src/tags-db/startCli.ts
@@ -2,13 +2,28 @@
 import WebSocket from 'ws'
 import ClientRepl from './ClientRepl'
 import CommandConnection from './CommandConnection'
+import Minimist from 'minimist'
 
 export default async function main() {
+    const cliArgs = Minimist(process.argv.slice(2), {
+    });
+
+    console.log('cliArgs: ', cliArgs);
+
     const ws = new WebSocket('http://localhost:42940');
     await new Promise((resolve, reject) => {
         ws.on('open', resolve);
     });
+
     const commandConnection = new CommandConnection(ws);
+
+    if (cliArgs._.length > 0) {
+        const command = cliArgs._.join(' ');
+
+        const result = await commandConnection.run(command);
+        commandConnection.close();
+        return;
+    }
 
     const repl = new ClientRepl(commandConnection);
     repl.start();
