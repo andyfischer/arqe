80f97fbb017fea646816c0a8e45fca3c69d5ebf5
Andrew Fischer <andy.fischer@gmail.com>
2019-12-18 13:44:27 -0700

    wip on listening

---
+++ src/socket/SocketConnection.ts
@@ -0,0 +1,70 @@
+
+import WebSocket from 'ws'
+import { createUniqueEntity } from '../GraphORM'
+import Graph from '../Graph'
+import GraphContext from '../GraphContext'
+import logError from '../logError'
+import parseCommand from '../parseCommand'
+import Command from '../Command'
+
+export default class SocketConnection {
+    ws: WebSocket
+    graph: Graph
+    graphContext: GraphContext
+
+    send(data) {
+        this.ws.send(JSON.stringify(data))
+    }
+
+    listenCommand(reqid: string, command: Command) {
+    }
+
+    async handleCommand(reqid: string, command: Command) {
+        command.respond = (result) => {
+            this.send({reqid, result});
+        }
+
+        command.respondPart = (result) => {
+            this.send({reqid, result, more: true});
+        }
+
+        command.respondEnd = () => {
+            this.send({reqid});
+        }
+
+        await this.graphContext.handleCommand(command)
+    }
+
+    constructor(graph: Graph, ws: WebSocket) {
+        this.graph = graph
+        this.ws = ws;
+
+        const id = createUniqueEntity(graph, 'connection')
+        this.graphContext = new GraphContext(this.graph);
+        this.graphContext.addOptionalContextTag({ tagType: 'connection', tagValue: id });
+
+        ws.on('message', async (str) => {
+            const data = JSON.parse(str);
+            const { reqid, command } = data;
+
+            try {
+                const parsedCommand = parseCommand(command);
+
+                if (parsedCommand.command === 'listen') {
+                    this.listenCommand(reqid, parsedCommand);
+                }
+
+                this.handleCommand(reqid, parsedCommand);
+
+            } catch (err) {
+                logError(err);
+                this.ws.send(JSON.stringify({reqid, internalError: true, err}));
+            }
+        });
+
+        ws.on('close', async (str) => {
+            this.graph.handleCommandStr(`delete connection/${id} *`)
+            console.log(`server: closed connection/${id}`);
+        });
+    }
+}
--- src/socket/SocketServer.ts
+++ src/socket/SocketServer.ts
@@ -2,9 +2,7 @@
 import WebSocket from 'ws'
 import Graph from '../Graph'
 import GraphContext from '../GraphContext'
-import parseCommand from '../parseCommand'
-import { createUniqueEntity } from '../GraphORM'
-import logError from '../logError'
+import SocketConnection from './SocketConnection'
 
 export default class ServerSocket {
     graph: Graph
@@ -15,47 +13,7 @@ export default class ServerSocket {
         this.wss = wss;
 
         this.wss.on('connection', (ws) => {
-
-            const id = createUniqueEntity(graph, 'connection')
-
-            const graphContext = new GraphContext(this.graph);
-            graphContext.addOptionalContextTag({ tagType: 'connection', tagValue: id });
-
-            ws.on('message', async (str) => {
-                const data = JSON.parse(str);
-                const { reqid, command } = data;
-
-                function send(data) {
-                    ws.send(JSON.stringify(data))
-                }
-
-                try {
-                    const parsedCommand = parseCommand(command);
-
-                    parsedCommand.respond = (result) => {
-                        send({reqid, result});
-                    }
-
-                    parsedCommand.respondPart = (result) => {
-                        send({reqid, result, more: true});
-                    }
-
-                    parsedCommand.respondEnd = () => {
-                        send({reqid});
-                    }
-
-                    await graphContext.handleCommand(parsedCommand)
-
-                } catch (err) {
-                    logError(err);
-                    ws.send(JSON.stringify({reqid, internalError: true, err}));
-                }
-            });
-
-            ws.on('close', async (str) => {
-                graph.handleCommandStr(`delete connection/${id} *`)
-                console.log(`server: closed connection/${id}`);
-            });
+            const socketConnection = new SocketConnection(this.graph, ws);
         });
     }
 }
