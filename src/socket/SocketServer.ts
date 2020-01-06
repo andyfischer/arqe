4ff42f080351ec6ca7f13d84a39b82665da2ab0c
Andrew Fischer <andy.fischer@gmail.com>
2020-01-06 12:23:27 -0700

    some code cleanup

--- src/socket/SocketServer.ts
+++ src/socket/SocketServer.ts
@@ -2,7 +2,55 @@
 import WebSocket from 'ws'
 import Graph from '../Graph'
 import GraphContext from '../GraphContext'
-import SocketConnection from './SocketConnection'
+import { createUniqueEntity } from '../GraphORM'
+import Command from '../Command'
+import parseCommand from '../parseCommand'
+import logError from '../logError'
+
+class Connection {
+    ws: WebSocket
+    graph: Graph
+    graphContext: GraphContext
+
+    send(data) {
+        this.ws.send(JSON.stringify(data))
+    }
+
+    async handleCommand(reqid: string, command: Command) {
+        await this.graphContext.handleCommand(command, (msg) => {
+            this.send({reqid, msg});
+        });
+    }
+
+    constructor(graph: Graph, ws: WebSocket) {
+        this.graph = graph
+        this.ws = ws;
+
+        const id = createUniqueEntity(graph, 'connection');
+
+        this.graphContext = new GraphContext(this.graph);
+        this.graphContext.addOptionalContextTag({ tagType: 'connection', tagValue: id });
+
+        ws.on('message', async (str) => {
+            const data = JSON.parse(str);
+            const { reqid, command } = data;
+
+            try {
+                const parsedCommand = parseCommand(command);
+                this.handleCommand(reqid, parsedCommand);
+
+            } catch (err) {
+                logError(err);
+                this.ws.send(JSON.stringify({reqid, internalError: true, err}));
+            }
+        });
+
+        ws.on('close', async (str) => {
+            this.graph.run(`delete connection/${id} *`)
+            console.log(`server: closed connection/${id}`);
+        });
+    }
+}
 
 export default class ServerSocket {
     graph: Graph
@@ -13,7 +61,7 @@ export default class ServerSocket {
         this.wss = wss;
 
         this.wss.on('connection', (ws) => {
-            const socketConnection = new SocketConnection(this.graph, ws);
+            const socketConnection = new Connection(this.graph, ws);
         });
     }
 }
