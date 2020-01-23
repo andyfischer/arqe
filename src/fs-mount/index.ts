c214af6daf3bf677b33a253b7d7316c318d14d75
Andrew Fischer <andy.fischer@gmail.com>
2020-01-23 11:50:05 -0700

    start dashboard

---
+++ src/fs-mount/index.ts
@@ -0,0 +1,9 @@
+
+// use fsevents, start listening for changes
+// implement a custom storage backend
+// respond with file contents
+// write 'change' events on file changes
+
+// mount from db to filesystem:
+//  Copy all files on start
+//  Listen for changes and write updates
