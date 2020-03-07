13cddb123a15706d3830dcbe46a5814510f2a7ee
Andrew Fischer <andy.fischer@gmail.com>
2020-03-07 14:31:22 -0700

    support multiple code generation targets

---
+++ dashboard/src/Spreadsheet.tsx
@@ -0,0 +1,29 @@
+
+// goals:
+//   take input data about rows & columns & cells
+//   render grid
+//   adjust sizes based on item contents
+//   cell selection
+//   change selection with arrows
+//   start editing a cell
+//   press enter to finish editing a cell
+//   click and drag cell edges
+
+// SpreadsheetView
+// SpreadsheetState
+//   as Graph
+
+// Data model
+// 
+//   col/x
+//   col/x width
+//   col/x name
+//   row/x
+//   row/x height
+//   spreadsheet
+//   spreadsheet row/*
+//   spreadsheet col/*
+//   row/x col/x cell
+
+//   spreadsheet cursor row/x col/x
+//   spreadsheet edit-state/editing
