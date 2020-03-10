356bf10b3f9c7f32cf068687f593a97c962a4956
Andrew Fischer <andy.fischer@gmail.com>
2020-03-10 15:56:53 -0700

    add file GeneratedApi

---
+++ src/grind-test/GeneratedApi.ts
@@ -0,0 +1,13 @@
+import { Graph, Relation } from '..'
+
+export default class API {
+    graph: Graph
+
+    constructor(graph: Graph) {
+        this.graph = graph;
+    }
+    
+    run(command: string) {
+        this.graph.run(command);
+    }
+}
