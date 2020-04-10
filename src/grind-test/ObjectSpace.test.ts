273d7a8d081875050f5a00a3a199d448004f9bf4
Andrew Fischer <andy.fischer@gmail.com>
2020-04-09 21:07:02 -0700

    add test

--- src/grind-test/ObjectSpace.test.ts
+++ src/grind-test/ObjectSpace.test.ts
@@ -15,3 +15,16 @@ test('can get on an object-space column', async ({run}) => {
     await run('set ot/ot1 attr1/test')
     expect(await run('get ot/ot1 attr1/*')).toEqual(['attr1/test']);
 });
+
+test('ObjectSpace attributes exclude multiple values', async ({run}) => {
+
+    await run('set object-type/ot');
+    await run('set object-type/ot attribute attr1');
+    await run('set ot/ot2');
+
+    expect(await run('get ot/ot2 attr1/*')).toEqual([]);
+    await run('set ot/ot2 attr1/1');
+    expect(await run('get ot/ot2 attr1/*')).toEqual(["attr1/1"]);
+    await run('set ot/ot2 attr1/2');
+    expect(await run('get ot/ot2 attr1/*')).toEqual(["attr1/2"]);
+});
