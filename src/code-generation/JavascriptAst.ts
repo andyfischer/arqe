48a3716040c22924071f7ac4a802e08acf115fbe
Andrew Fischer <andy.fischer@gmail.com>
2020-04-29 22:15:51 -0700

    add JavascriptAst

---
+++ src/code-generation/JavascriptAst.ts
@@ -0,0 +1,247 @@
+
+
+class Block {
+    blockType: string
+    statements: Statement[] = []
+    parent: Statement
+
+    constructor(blockType: string, parent: Statement) {
+        this.blockType = blockType;
+        this.parent = parent;
+    }
+
+    stringify() {
+        return stringifyBlock(this);
+    }
+
+    addImport(lhs: string, rhs: string) {
+        this.statements.push(new ImportStatement(lhs, rhs));
+    }
+
+    addClass(className: string) {
+        const def = new ClassDef(className);
+        this.statements.push(def);
+        return def;
+    }
+
+    addRaw(text: string) {
+        this.statements.push(new RawStatement(text));
+    }
+}
+
+class ImportStatement implements Statement {
+    lhs: string
+    rhs: string
+
+    constructor(lhs, rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    line() {
+        return `import ${this.lhs} from "${this.rhs}"`
+    }
+}
+
+class RawStatement {
+    text: string
+
+    constructor(text: string) {
+        this.text = text;
+    }
+
+    line() {
+        return this.text;
+    }
+}
+
+class FunctionInputDef {
+    name: string
+    tsType?: string
+}
+
+interface Statement {
+    line: () => string
+    contents?: Block
+}
+
+class Whitespace implements Statement {
+    line() {
+        return ''
+    }
+}
+
+class StringStatement implements Statement {
+    line() {
+        return ''
+    }
+}
+
+class ClassDef implements Statement {
+    name: string
+    contents: Block
+
+    constructor(name: string) {
+        this.name = name;
+        this.contents = new Block('class', this)
+    }
+
+    addField(name: string, tsType: string) {
+        this.contents.statements.push(new FieldDecl(name, tsType));
+    }
+
+    addMethod(name: string) {
+        const decl = new FunctionDecl('method', name);
+        this.contents.statements.push(decl);
+        return decl;
+    }
+
+    line() {
+        return `class ${this.name}`
+    }
+}
+
+class FieldDecl implements Statement {
+    name: string
+    tsType?: string
+    initializerExr: string
+
+    constructor(name: string, tsType: string) {
+        this.name = name;
+        this.tsType = tsType;
+    }
+
+    line() {
+        let s = `${this.name}: ${this.tsType}`
+        if (this.initializerExr)
+            s += ` = ${this.initializerExr}`;
+        return s;
+    }
+}
+
+class FunctionDecl implements Statement {
+    statementType = 'functionDecl'
+    format: string
+    name: string
+    inputs: { name: string, tsType: string }[] = []
+    outputType: string
+    contents: Block
+
+    constructor(format: string, name: string) {
+        this.format = format;
+        this.name = name;
+        this.contents = new Block('function', this)
+    }
+    
+    addInput(name: string, tsType: string) {
+        this.inputs.push({name, tsType});
+    }
+
+    setOutputType(tsType: string) {
+        this.outputType = tsType;
+    }
+
+    line() {
+        const inputs = this.inputs.map(input => {
+            let s = input.name;
+            if (input.tsType)
+                s += ': ' + input.tsType;
+            return s;
+        }).join(', ');
+
+        let str;
+
+        if (this.format === 'method')
+            str = `${this.name}(${inputs})`
+        else
+            str = `function ${this.name}(${inputs})`
+
+        if (this.outputType)
+            str += ': ' + this.outputType;
+
+        return str;
+    }
+}
+
+export function startFile() {
+    return new Block('file', null);
+}
+
+class LineWriter {
+    currentIndent: number = 0
+    indentStr: string
+    out: string[] = []
+
+    writeln(s: string) {
+        if (this.out.length > 0)
+            this.out.push('\n');
+
+        if (s) {
+            this.out.push(this.indentStr);
+            this.out.push(s);
+        }
+    }
+
+    indent() {
+        this.currentIndent += 1;
+        this.indentStr = '    '.repeat(this.currentIndent);
+    }
+
+    unindent() {
+        if (this.currentIndent == 0)
+            throw new Error(`can't unindent at 0`);
+
+        this.currentIndent -= 1;
+        this.indentStr = '    '.repeat(this.currentIndent);
+    }
+
+    stringify() {
+        return this.out.join('');
+    }
+}
+
+function* iterateBlock(block: Block) {
+    for (const statement of block.statements) {
+        yield ({ statement })
+
+        if (statement.contents) {
+            yield ({ startBlock: statement.contents })
+            yield* iterateBlock(statement.contents);
+            yield ({ finishBlock: statement.contents })
+        }
+    }
+}
+
+export function stringifyBlock(block: Block) {
+    const out = [];
+    const writer = new LineWriter();
+
+    for (const it of iterateBlock(block)) {
+
+        if (it.startBlock) {
+            writer.indent()
+        }
+
+        if (it.statement) {
+            if (it.statement.statementType === 'functionDecl') {
+                writer.writeln('')
+            }
+
+            let line = it.statement.line();
+
+            if (it.statement.contents) {
+                line += ' {';
+            }
+
+            writer.writeln(line);
+        }
+
+        if (it.finishBlock) {
+            writer.unindent()
+            writer.writeln('}');
+        }
+    }
+
+    return writer.stringify();
+}
+
---
+++ src/code-generation/__tests__/JavascriptAst.test.ts
@@ -0,0 +1,28 @@
+
+import { startFile } from '../JavascriptAst'
+
+it('can declare an import', () => {
+    const file = startFile();
+
+    file.addImport('{ GraphLike, Relation, receiveToRelationListPromise }', '..');
+
+    expect(file.stringify()).toEqual(`import { GraphLike, Relation, receiveToRelationListPromise } from ".."`);
+});
+
+it('can declare a class', () => {
+    const file = startFile();
+    const clss = file.addClass('API');
+    clss.addField('graph', 'GraphLike');
+    const c = clss.addMethod('constructor');
+    c.addInput('graph', 'GraphLike');
+    c.contents.addRaw('this.graph = graph;')
+
+    expect(file.stringify()).toEqual(
+`class API {
+    graph: GraphLike
+
+    constructor(graph: GraphLike) {
+        this.graph = graph;
+    }
+}`);
+});
