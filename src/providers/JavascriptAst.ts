1e71e91bd3f2d497fdccd1c5bc7be67b5d79de90
Andrew Fischer <andy.fischer@gmail.com>
2020-05-24 14:49:47 -0700

    working on javascript AST

--- src/providers/JavascriptAst.ts
+++ src/providers/JavascriptAst.ts
@@ -5,8 +5,23 @@
 // js-ast block/* line/*
 
 import Esprima from 'esprima'
+import IDSource from '../utils/IDSource'
 
 // get javascript-ast text(...)
 
+interface JavascriptAst {
+}
+
 class JavascriptAstProvider {
+
+    nextInstanceId = new IDSource()
+    instances: { [id: string]: JavascriptAst } = {}
+
+    createAstFromText(text: string) {
+        const id = nextInstanceId.take();
+        const ast = Esprima.parse(text);
+        instances[id] = ast;
+        return id;
+    }
+
 }
