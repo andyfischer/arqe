bd62bde81c0166f3e718799dbf7446cfc244f1d2
Andrew Fischer <andy.fischer@gmail.com>
2020-06-17 09:49:53 -0700

    tiny clean

--- src/CommandStep.ts
+++ src/CommandStep.ts
@@ -3,8 +3,6 @@ import Command, { CommandFlags } from './Command'
 import Graph from './Graph'
 import Tuple from './Tuple'
 import Pattern from './Pattern'
-import Stream from './Stream'
-import { receiveToTupleList } from './receiveUtils'
 import Pipe from './Pipe'
 
 export default class CommandStep {
@@ -23,22 +21,4 @@ export default class CommandStep {
         this.flags = command.flags;
         this.pattern = command.toPattern();
     }
-
-    /*
-    toRelationSearch(): SearchOperation {
-
-        if (!this.output)
-            throw new Error('missing this.output');
-
-        const output = this.output;
-
-        return {
-            graph: this.graph,
-            pattern: this.pattern,
-            subSearchDepth: 0,
-            next(r) { output.next(r) },
-            done() { output.done() },
-        }
-    }
-    */
 }
